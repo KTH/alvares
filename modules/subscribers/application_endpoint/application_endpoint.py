6053b555c5a8bc8da2737ba66a9faa28d659640b
Patric Jansson <paddy@kth.se>
2019-03-05 23:39:03 +0100

    Link to Flottsbro

--- modules/subscribers/application_endpoint/application_endpoint.py
+++ modules/subscribers/application_endpoint/application_endpoint.py
@@ -11,8 +11,12 @@ def unsubscribe():
     unsubscribe_from_event('deployment', handle_deployment)
 
 def handle_deployment(deployment):
+
+    call_slack_channel_with_application_flottsbro_endpoint_url(deployment)
+        
     if deployment_util.has_application_path(deployment):
         call_slack_channel_with_application_endpoint_url(deployment)
+
 
 def call_slack_channel_with_application_endpoint_url(deployment):
     message = (f'*{deployment_util.get_friendly_name(deployment)}* '
@@ -23,6 +27,16 @@ def call_slack_channel_with_application_endpoint_url(deployment):
         slack_util.call_slack_endpoint(channel,
                                        environment.get_env(environment.SLACK_WEB_HOOK),
                                        create_slack_payload(message, channel))
+
+def call_slack_channel_with_application_flottsbro_endpoint_url(deployment):
+    message = (f':information_source: About *{deployment_util.get_friendly_name(deployment)}* '
+               f'https://app.kth.se/pipeline/#{deployment_util.get_application_name(deployment)}')
+
+    for channel in deployment_util.get_slack_channels(deployment):
+        slack_util.call_slack_endpoint(channel,
+                                       environment.get_env(environment.SLACK_WEB_HOOK),
+                                       create_slack_payload(message, channel))
+
 
 def create_slack_payload(message, channel):
     return {
