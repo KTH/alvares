80bf3a62733e257bd4feb6a034f6957ec06f817c
Patric Jansson <paddy@kth.se>
2019-03-07 15:42:53 +0100

    Schema validation for enriched deployments written to db.

--- test/test_json_schema.py
+++ test/test_json_schema.py
@@ -4,6 +4,9 @@ import unittest
 from test import mock_data
 import requests
 from modules import environment
+from modules import deployment_util
+from modules import deployment_enricher
+
 
 class TestSchemaValidation(unittest.TestCase):
 
@@ -16,6 +19,18 @@ class TestSchemaValidation(unittest.TestCase):
             result = requests.post(furano_url, json=sample, allow_redirects=False)
             self.assertEqual(result.json(), {})
             self.assertEqual(result.status_code, 200)
+
+    @unittest.skipIf(environment.get_env(environment.SKIP_VALIDATION_TESTS),
+                     'SKIP_VALIDATION_TESTS set')
+    def test_validate_deployment_samples_enriched(self):
+        furano_url = environment.get_env_with_default_value(environment.VALIDATE_DEPLOYMENT_URL, 'https://app.kth.se/jsonschema/alvares/deployment')
+        for sample in mock_data.get_deployment_samples():
+            sample = deployment_enricher.enrich(sample)
+            del sample["mock-expected"]
+            result = requests.post(furano_url, json=sample, allow_redirects=False)
+            self.assertEqual(result.json(), {})
+            self.assertEqual(result.status_code, 200)
+
 
     @unittest.skipIf(environment.get_env(environment.SKIP_VALIDATION_TESTS),
                      'SKIP_VALIDATION_TESTS set')
@@ -41,3 +56,4 @@ class TestSchemaValidation(unittest.TestCase):
             result = requests.post(validation_url, json=sample, allow_redirects=False)
             self.assertEqual(result.json(), {})
             self.assertEqual(result.status_code, 200)
+
