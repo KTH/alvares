99c54f049e0d106215833bb6a070eb2b93dff6e2
Patric Jansson <paddy@kth.se>
2019-04-24 10:05:50 +0200

    test fix

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-EXIST=`command -v pipenv`
-
-if [ -z $EXIST ]; then
-    pipenv install --dev 
-else 
-    echo "pipenv install --dev already run"; 
-fi
+pipenv install --dev 
 
 pipenv run green -vv --run-coverage --failfast "test" "$@"
