e0d8b2585fde380ada72a32873a8065a24f2bff2
Patric Jansson <paddy@kth.se>
2020-03-30 18:07:37 +0200

    Log feature flag

--- modules/feature_flags.py
+++ modules/feature_flags.py
@@ -1,10 +1,14 @@
 __author__ = 'tinglev@kth.se'
 
+import logging
 from modules import environment
+
+LOG = logging.getLogger(__name__)
 
 def use_lofsdalen():
     return use('FEATURE_FLAG_LOFSDALEN')
 
 def use(flag):
     value = environment.get_env_with_default_value(flag, False)
+    LOG.info('Feature flag %s is %s.', flag, value)
     return environment.is_true(value)
